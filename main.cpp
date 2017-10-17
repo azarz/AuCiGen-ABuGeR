62481d11f4e65f4bc5535e59729b932d9b70651d
VictorLambert <vict.lambert@yahoo.fr>
2017-10-17 16:23:17 +0200

    open shp class

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include "shpto3d.h"
 
 using namespace std;
 
 int main()
 {
+    shpto3d("/home", "toto");
     cout << "Hello world!" << endl;
     return 0;
 }
---
+++ src/shpto3d.cpp
@@ -0,0 +1,17 @@
+#include "shpto3d.h"
+#include <iostream>
+using namespace std;
+
+shpto3d::shpto3d(std::string fill_path, std::string fill_name)
+{
+    name=fill_name;
+    path=fill_path+"/"+fill_name+".shp";
+    cout << "ok" << endl;
+    cout << name << endl;
+    cout << path << endl;
+}
+
+shpto3d::~shpto3d()
+{
+    cout << "fin" << endl;
+}
