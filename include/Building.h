c4acbd04944e15cc624408e7491be734e96221b9
LSchlegel <ludivine.sylvie@hotmail.fr>
2017-10-19 12:41:22 +0200

    add Building class

---
+++ include/Building.h
@@ -0,0 +1,21 @@
+#ifndef BUILDING_H
+#define BUILDING_H
+
+#include "parcel.h"
+#include "BuildingModel.h"
+#include <vector>
+
+class Building
+{
+    public:
+        Building();
+        virtual ~Building();
+
+        Parcel *parcel;
+        std::vector<BuildingModel> building_model;
+
+    protected:
+    private:
+};
+
+#endif // BUILDING_H
