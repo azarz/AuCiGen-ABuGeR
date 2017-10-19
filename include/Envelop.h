0911f8fb049dbc3bf7900eab13b966809d05b47d
LSchlegel <ludivine.sylvie@hotmail.fr>
2017-10-19 12:17:03 +0200

    update Envelop class

--- include/Envelop.h
+++ include/Envelop.h
@@ -1,12 +1,22 @@
 #ifndef ENVELOP_H
 #define ENVELOP_H
 
+#include "BuildingModel.h"
+#include "FootPrint.h"
+#include "parcel.h"
 
 class Envelop
 {
     public:
         Envelop();
         virtual ~Envelop();
+
+
+
+        BuildingModel *volume;
+        FootPrint *footprint;
+        Parcel *parcel;
+        int n_floor;
     protected:
     private:
 };
