20b951f452272a886bd7c5207d51a710d44c15c3
LSchlegel <ludivine.sylvie@hotmail.fr>
2017-10-19 16:32:50 +0200

    creat OpenShapeFile function, update parcel class : function creat_footprint

--- include/open_shp.h
+++ include/open_shp.h
@@ -1,6 +1,120 @@
 #ifndef OPEN_SHP_INCLUDED
 #define OPEN_SHP_INCLUDED
 
+#include <iostream>
+#include "ogrsf_frmts.h"
+
+using namespace std;
+
+
+void OpenShapeFile(string fill_directory, char layer_type)
+{
+    OGRErr error;
+    GDALAllRegister();
+
+    GDALDataset       *poDS;
+    poDS = (GDALDataset*) GDALOpenEx( fill_directory, GDAL_OF_VECTOR, NULL, NULL, NULL );
+    if( poDS == NULL )
+    {
+        printf( "Open failed.\n" );
+        exit( 1 );
+    }
+    OGRLayer  *poLayer;
+    if (layer_type == "R")
+    {poLayer = poDS->GetLayerByName( "Road" );}
+    else
+    {poLayer = poDS->GetLayerByName( "Parcel" );}
+
+
+
+OGRwkbGeometryType LayerGeometryType = poLayer ->GetGeomType();
+int NumberOfFeatures = poLayer ->GetFeatureCount(true);
+poLayer ->ResetReading();
+
+//Polygon Shapefile
+if ( wkbFlatten ( LayerGeometryType ) == wkbPolygon )
+{
+   OGRFeature *poFeature;
+   PolygonFeature Polygon;
+   OGRPoint ptTemp;
+   for ( int i = 0; i < NumberOfFeatures; i++ )
+   {
+       printf("\nelement : %d\n", i);
+       poFeature = poLayer ->GetNextFeature();
+       OGRGeometry *poGeometry;
+       poGeometry = poFeature ->GetGeometryRef();
+       if ( poGeometry != NULL && wkbFlatten ( poGeometry ->getGeometryType() ) == wkbPolygon )
+       {
+           OGRPolygon *poPolygon = ( OGRPolygon * )poGeometry;
+           OGRFeatureDefn *poFDefn = poLayer->GetLayerDefn();
+           int iField =0;
+            OGRFieldDefn *poFieldDefn = poFDefn->GetFieldDefn( iField );
+            if (layer_type == "R")
+            {
+                if( poFieldDefn->GetType() == OFTString )
+                {
+                    Road road = new Road(poPolygon, poFeature->GetFieldAsString(iField));
+                }
+            }
+            else
+            {Parcel parcel = new Parcel(poPolygon);}
+
+
+       }
+
+   }
+   OGRFeature::DestroyFeature(poFeature);
+}
+
+GDALClose( poDS );
+}
+
+int main()
+{
+    OpenShapeFile();
+    cout << "Hello world!" << endl;
+    return 0;
+}
 
 
 #endif // OPEN_SHP_INCLUDED
+
+/*
+nt main()
+{
+    GDALAllRegister();
+
+    GDALDataset       *poDS;
+    //poDS = (GDALDataset*) GDALOpenEx( "data/geoflar-departementsLAM.shp", GDAL_OF_VECTOR, NULL, NULL, NULL );
+    poDS = (GDALDataset*) GDALOpenEx( "data/test_shp/test_shpLAM.shp", GDAL_OF_VECTOR, NULL, NULL, NULL );
+    if( poDS == NULL )
+    {
+        printf( "Open failed.\n" );
+        exit( 1 );
+    }
+    OGRLayer  *poLayer;
+    //poLayer = poDS->GetLayerByName( "geoflar-departementsLAM" );
+    poLayer = poDS->GetLayerByName( "test_shpLAM" );
+    OGRFeature *poFeature;
+    poLayer->ResetReading();
+    while( (poFeature = poLayer->GetNextFeature()) != NULL )
+    {
+        OGRFeatureDefn *poFDefn = poLayer->GetLayerDefn();
+        int iField;
+        //iField =0;
+        for( iField = 0; iField < poFDefn->GetFieldCount(); iField++ )
+        {
+            //printf( "place : %d", iField );
+            OGRFieldDefn *poFieldDefn = poFDefn->GetFieldDefn( iField );
+            if( poFieldDefn->GetType() == OFTInteger )
+                printf( "%d,", poFeature->GetFieldAsInteger( iField ) );
+            else if( poFieldDefn->GetType() == OFTInteger64 )
+                printf( CPL_FRMT_GIB ",", poFeature->GetFieldAsInteger64( iField ) );
+            else if( poFieldDefn->GetType() == OFTReal )
+                printf( "%.3f,", poFeature->GetFieldAsDouble(iField) );
+            else if( poFieldDefn->GetType() == OFTString )
+                printf( "%s,", poFeature->GetFieldAsString(iField) );
+            else
+                printf( "%s,", poFeature->GetFieldAsString(iField) );
+        }
+*/
--- src/parcel.cpp
+++ src/parcel.cpp
@@ -22,15 +22,15 @@ Parcel::~Parcel()
 OGRLinearRing Parcel::create_footprint(OGRLineString linearIntersection, OGRLineString otherSides)
 {
     vector<double> margins = type->get_margin();
-    OGRPolygon road_buffer = linearIntersection.Buffer(margins.at(1));
-    OGRPolygon neigh_buffer = otherSides.Buffer(margins.at(0));
+    OGRGeometry* road_buffer = linearIntersection.Buffer(margins.at(1));
+    OGRGeometry* neigh_buffer = otherSides.Buffer(margins.at(0));
 
-    OGRPolygon diff1 = Parcel.geom.Difference(road_buffer);
-    OGRPolygon diff2 = diff1.Difference(neigh_buffer);
+    OGRGeometry* diff1 = geom.Difference(road_buffer);
+    OGRGeometry* diff2 = diff1->Difference(neigh_buffer);
 
-    OGRLinearRing contour = diff2.getLinearGeometry();
-
-    return contour;
+    OGRGeometry* contour = diff2->getLinearGeometry();
+    OGRLinearRing *contour2 = (OGRLinearRing *) contour;
+    return contour2;
 }
 
 void Parcel::to_obj()
