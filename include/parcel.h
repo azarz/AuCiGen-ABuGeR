4db7bb22bf10ea0dd5975b8ceed19320b03b4ece
LSchlegel <ludivine.sylvie@hotmail.fr>
2017-10-19 11:47:18 +0200

    update parcel class

--- include/parcel.h
+++ include/parcel.h
@@ -7,17 +7,10 @@ class Parcel
     public:
         Parcel();
         virtual ~Parcel();
-        double C_area();
-        double C_area_price();
-        double C_floor_price();
-        int C_n_floor();
-        double C_height();
         void create_footprint();
         double area;
         double area_price;
         double floor_price;
-        int n_floor;
-        double height;
     protected:
     private:
 };
--- src/parcel.cpp
+++ src/parcel.cpp
@@ -15,25 +15,7 @@ Parcel::~Parcel()
     //dtor
     cout << "del" << endl;
 }
-double Parcel::C_area()
-{
-    //calculate the parcel area
-}
-double Parcel::C_area_price()
-{
-    //calculate the area price
-}
-double Parcel::C_floor_price()
-/** Bonjour
-*:param: dgdgdg
-*/
-{
-    //calculate the area need for the rentability
-}
-int Parcel::C_n_floor()
-{
-    //calculate the number of floor
-}
+
 void Parcel::create_footprint()
 {
     //create the foot print of building
