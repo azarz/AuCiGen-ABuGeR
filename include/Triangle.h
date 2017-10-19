c9843039d5f1967adb77d67e1b30aa821f0f3608
LSchlegel <ludivine.sylvie@hotmail.fr>
2017-10-19 12:34:04 +0200

    update Triangle class

--- include/Triangle.h
+++ include/Triangle.h
@@ -1,12 +1,23 @@
 #ifndef TRIANGLE_H
 #define TRIANGLE_H
 
+#include "Point.h"
+#include <iostream>
+
+using namespace std;
 
 class Triangle
 {
     public:
         Triangle();
         virtual ~Triangle();
+
+
+
+        string type; //wall, floor, roof
+        Point *p1;
+        Point *p2;
+        Point *p3;
     protected:
     private:
 };
