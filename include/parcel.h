3d12232893a6e711b876c18695a44459bbe8e177
LSchlegel <ludivine.sylvie@hotmail.fr>
2017-10-19 12:00:02 +0200

    update parcel class

--- include/parcel.h
+++ include/parcel.h
@@ -1,16 +1,24 @@
 #ifndef PARCEL_H
 #define PARCEL_H
 
+#include "ogrsf_frmts.h"
+#include "BuildingType.h"
 
 class Parcel
 {
     public:
         Parcel();
         virtual ~Parcel();
+
         void create_footprint();
+        void to_obj();
+
+        OGRPolygon geom;
+        BuildingType type;
         double area;
         double area_price;
         double floor_price;
+
     protected:
     private:
 };
