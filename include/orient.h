97d644ce1ee44f712a2cc60e3e5322b4966567a0
AZarzelli <amaury.zarzelli@gmail.com>
2017-10-19 14:29:10 +0200

    add orientaion function (without tests)

---
+++ include/orient.h
@@ -0,0 +1,29 @@
+#ifndef ORIENT_H_INCLUDED
+#define ORIENT_H_INCLUDED
+
+#include "ogrsf_frmts.h"
+
+/**
+* This function allows us to extract the linear geometry from a OGRPolygon.
+* @param[in] polygon It's the polygon of the parcel.
+* @return The linear geometry of the polygon.
+*/
+OGRLinearRing get_linear_geometry(OGRPolygon polygon);
+
+/**
+* This function allows us to get the intersection line between a linear ring and the road polygons.
+* @param[in] linearRing It's the contour of the parcel.
+* @param[in] road It's the polygons that represent the road.
+* @return The side in front of the largest road.
+*/
+OGRLineString get_intersection_road(OGRLinearRing linearRing, OGRPolygon road);
+
+/**
+* Get the other sides of the parcel contour
+* @param[in] linearRing It's the contour of the parcel.
+* @param[in] intersectionLine It's the side in front of the largest road.
+* @return The other sides of the parcel (without the side in front of the largest road).
+*/
+OGRLineString get_other_sides(OGRLinearRing linearRing, OGRLineString intersectionLine);
+
+#endif // ORIENT_H_INCLUDED
---
+++ src/orient.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "ogrsf_frmts.h"
+
+OGRLinearRing get_linear_geometry(OGRPolygon polygon)
+{
+    OGRGeometry linearRing = polygon.getLinearGeometry();
+    return linearRing;
+}
+
+OGRLineString get_intersection_road(OGRLinearRing linearRing, OGRPolygon road)
+{
+    //don't forget to test if there is an intersection (choice a side if not)
+    OGRLineString linearIntersection = linearRing.Intersection(road);
+    return linearIntersection;
+}
+
+OGRLineString get_other_sides(OGRLinearRing linearRing, OGRLineString intersectionLine)
+{
+    OGRLineString otherSides = linearRing.Difference(intersectionLine);
+    return otherSides;
+}
