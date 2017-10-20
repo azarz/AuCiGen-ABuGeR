87b20b68f17518d5e5aefbfa655593272ac27f8c
LSchlegel <ludivine.sylvie@hotmail.fr>
2017-10-20 12:10:24 +0200

    update main with relativ path

--- main.cpp
+++ main.cpp
@@ -7,15 +7,20 @@
 
 using namespace std;
 
+
+vector<Polygon> ROADS
+vector<Polygon> PARCELS
+
+
 int main()
 {
     //Parcel();
-    char* fill_directory ="/home/formation/AuCiGen/ABuGeR/1_data/shp_R.shp";
+    char* fill_directory ="1_data/shp_R.shp";
     char layer_type ='R';
-    vector<Polygon> ROADS=OpenShapeFile(fill_directory, layer_type);
-    fill_directory ="/home/formation/AuCiGen/ABuGeR/1_data/shp_P.shp";
+    ROADS=OpenShapeFile(fill_directory, layer_type);
+    fill_directory ="1_data/shp_P.shp";
     layer_type ='P';
-    vector<Polygon> PARCELS=OpenShapeFile(fill_directory, layer_type);
+    PARCELS=OpenShapeFile(fill_directory, layer_type);
     cout << "Hello world!" << endl;
     return 0;
 }
