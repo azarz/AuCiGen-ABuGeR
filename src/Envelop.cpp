feb1a155276ce7b81f8fd8ec057ba775ad1c1690
VictorLambert <vict.lambert@yahoo.fr>
2017-10-20 14:44:31 +0200

    added calcul n_floor in envelop ctor (commentary)

--- src/Envelop.cpp
+++ src/Envelop.cpp
@@ -3,6 +3,11 @@
 Envelop::Envelop()
 {
     //ctor
+
+    /*//calculating number of floors
+	double n_calc(( parcel->get_floorspace() )/( footprint->get_geom()->OGRCurvePolygon::get_Area() ));
+	int n_floor = static_cast<int>(n_calc); //converting from double to int
+    */
 }
 
 Envelop::~Envelop()
