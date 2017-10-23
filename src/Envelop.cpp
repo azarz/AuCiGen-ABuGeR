459785bee74cf7a1922724992bd4da1918bf55d4
iesseddik <ismael.esseddik@gmail.com>
2017-10-23 12:53:09 +0200

    devloping the envelop ctor 1

--- src/Envelop.cpp
+++ src/Envelop.cpp
@@ -1,10 +1,22 @@
 #include "Envelop.h"
 
-Envelop::Envelop()
+Envelop::Envelop()// have to add an arg Footprint&Envelop pointers to get an envelop from it
 {
     //ctor
 
-    /*//calculating number of floors
+    /*//setting the footprint pointer
+    Footprint* m_footprint = footprint;
+    */
+
+    /*//setting the parcel pointer
+    Parcel* m_parcel = footprint->parcel;
+    */
+
+    /*//setting the the buildingmodel pointer for volume
+    BuildingModel* m_volume = buildingmodel;
+    */
+
+    /*//calculating floor number
 	double n_calc(( parcel->get_floorspace() )/( footprint->get_geom()->OGRCurvePolygon::get_Area() ));
 	int n_floor = static_cast<int>(n_calc); //converting from double to int
     */
