260e3f03406f3205de7afe2135cf19b85f8400ab
LSchlegel <ludivine.sylvie@hotmail.fr>
2017-10-24 11:25:27 +0200

    Point class doc

--- include/Point.h
+++ include/Point.h
@@ -11,10 +11,12 @@ class Point
     public:
         Point();
         /**
+        *@fn Point::Point()
         *Simple créator Point()
         */
         Point(double p_x, double p_y, double p_z);
         /**
+        *@fn Point::Point()
         *Créator Point()
         *@param[in] p_x double : x coordinate
         *@param[in] p_y double : y coordinate
@@ -25,27 +27,46 @@ class Point
         //Getters
         double get_x()const{return x;};
         /**
-        *Point::get_x()
+        *@fn double Point::get_x()
         *give the x coordinate
         *@return x double
         */
         double get_y()const{return y;};
         /**
-        *Point::get_y()
+        *@fn double Point::get_y()
         *give the y coordinate
         *@return y double
         */
         double get_z()const{return z;};
         /**
-        *Point::get_z()
+        *@fn double Point::get_z()
         *give the z coordinate
         *@return z double
         */
 
         //Other functions
         void translate(double vec[3]);
+        /**
+        *@fn void Point::translate(double vec[3])
+        *translate the point with a 3d vector
+        *update the x, y, z coordiantes
+        *@param[in] vec vector<double> dim 3 : translate vector
+        */
         void rotate(double vec[3], double angle);
+        /**
+        *@fn void Point::rotate(double vec[3], double angle)
+        *rotate the point with a rotate axe and a angle
+        *update the x, y, z coordiantes
+        *@param[in] vec vector<double> dim 3 : rotate axe
+        *@param[in] angle double : rotate angle [rad]
+        */
         void size(double vec[3]);
+        /**
+        *@fn void Point::size(double vec[3])
+        *???
+        *update the x, y, z coordiantes
+        *@param[in] vec vector<double> dim 3 : ?????
+        */
 
     protected:
     private:
